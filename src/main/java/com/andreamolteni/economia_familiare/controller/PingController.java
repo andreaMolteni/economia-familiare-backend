8e5747c7bfb9a4aeb7842cdb8234dd4ad6f312a7
Andrea Molteni <bernard.montalbano@esternibisp.com>
2026-02-16 17:26:06 +0100

    fix

--- src/main/java/com/andreamolteni/economia_familiare/controller/PingController.java
+++ src/main/java/com/andreamolteni/economia_familiare/controller/PingController.java
@@ -1,13 +1,18 @@
 package com.andreamolteni.economia_familiare.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
+        logger.info("ping ricevuto. Mi tengo scetato!");
         return ResponseEntity.ok("ok");
     }
 }
